8fbbe428f88f1ab9ced9c142d4d6d06df5be130b
Carlos Casas <albertocasas05@gmail.com>
2016-09-01 14:31:04 -0700

    Add the text box and button

--- battleship.js
+++ battleship.js
@@ -34,6 +34,8 @@ for (i = 0; i < cols; i++) {
 		square.className = "boardSquare";
 
 		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
+		square.textContent = i + 1;
+		letterArray = ["A", "B", "C", "D"];
 
 		// set each grid square's coordinates: multiples of the current row or column number
 		var topPosition = j * squareSize;
